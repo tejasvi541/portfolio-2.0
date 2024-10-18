35d4ba5cf5438e7d40cf0113b11d751a86dea162
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-10-18 14:36:57 -0400

    Changing Hero and Bio Assest

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -28,7 +28,7 @@ const AboutPage = () => {
           <div className="flex flex-col gap-6 justify-center">
             {/* BIOGRAPHY IMAGE */}
             <Image
-              src="/bio.jpg"
+              src="/bio.png"
               alt="Biography Image"
               width={112}
               height={112}
